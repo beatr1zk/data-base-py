2ee41f55089a5c5394104b59f31b231d064b68a9
Beatriz <beatriz.kava@gmail.com>
2026-09-22 11:50:46 -0300

    fk-avaliacoes

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from db import tabela_restaurante, criar_restaurante, listar_restaurantes
+from db import tabela_restaurante, tabela_avaliacoes, criar_restaurante,  criar_avaliacao, listar_restaurantes, listar_avaliacoes
 
 tabela_restaurante()
+tabela_avaliacoes() #Cade esse caraio que não existia
 
 # criar_restaurante("Mada", "Italiana")
+# criar_restaurante("Coconono", "Tropical")
 
+# criar_avaliacao(1, "Heron", 4.8)
+
+print("\nRestaurantes:")
 listar_restaurantes()
+
+print("\nAvaliações:")
+listar_avaliacoes()
--- db.py
+++ db.py
@@ -24,15 +24,15 @@ def tabela_restaurante():
     conexao.commit()
     conexao.close()
 
-def tabela_avaliacao():
+def tabela_avaliacoes():
     conexao = conectar()
     cursor = conexao.cursor()
     criar_tabela_avaliacao = """
         CREATE TABLE IF NOT EXISTS avaliacoes(
             id INT AUTO_INCREMENT PRIMARY KEY,
-            nome_usuario VARCHAR(100) NOT NULL,
-            nota FLOAT NOT NULL,
             id_restaurante INT NOT NULL,
+            nome_usuario VARCHAR(100) NOT NULL,
+            nota FLOAT(2,1) NOT NULL,
             FOREIGN KEY (id_restaurante) REFERENCES restaurantes(id)
             )
     """
@@ -50,6 +50,16 @@ def criar_restaurante(nome, categoria):
     conexao.commit()
     conexao.close()
 
+def criar_avaliacao(id_restaurante, nome_usuario, nota):
+    conexao = conectar()
+    cursor = conexao.cursor()
+    cursor.execute("""
+        INSERT INTO avaliacoes(id_restaurante, nome_usuario, nota)
+        VALUES (%s, %s, %s)
+    """, (id_restaurante, nome_usuario, nota))
+    conexao.commit()
+    conexao.close()
+
 def listar_restaurantes():
     conexao = conectar()
     cursor = conexao.cursor()
@@ -59,5 +69,44 @@ def listar_restaurantes():
     restaurantes = cursor.fetchall()
     for restaurante in restaurantes:
         print(restaurante)
+    conexao.commit()
+    conexao.close()
+
+def listar_avaliacoes(): 
+    conexao = conectar()
+    cursor = conexao.cursor()
+    cursor.execute("""
+        SELECT restaurantes.nome , avaliacoes.nome_usuario, avaliacoes.nota FROM avaliacoes
+        JOIN restaurantes ON avaliacoes.id_restaurante = restaurantes.id
+    """)
+    avaliacoes = cursor.fetchall()
+    for avaliacao in avaliacoes:
+        print(avaliacao)
+    conexao.commit()
+    conexao.close()
+
+def tabela_item_cardapio():
+    conexao = conectar()
+    cursor = conexao.cursor()
+    criar_tabela_item_cardapio = """
+        CREATE TABLE IF NOT EXISTS item_cardapio(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            id_restaurante INT NOT NULL,
+            nome_item VARCHAR(100) NOT NULL,
+            preco FLOAT(6,2) NOT NULL,
+            FOREIGN KEY (id_restaurante) REFERENCES restaurantes(id)
+            )
+    """
+    cursor.execute(criar_tabela_item_cardapio)
+    conexao.commit()
+    conexao.close()
+
+def criar_avaliacao(id_restaurante, nome_item, preco):
+    conexao = conectar()
+    cursor = conexao.cursor()
+    cursor.execute("""
+        INSERT INTO item_cardapio(id_restaurante, nome_item, preco)
+        VALUES (%s, %s, %s)
+    """, (id_restaurante, nome_item, preco))
     conexao.commit()
     conexao.close()
